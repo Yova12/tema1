1a8295d860350cc5976abdd04ff9d6a71bd9917c
Yova12 <97071733+Yova12@users.noreply.github.com>
2022-04-28 18:33:30 -0500

    Add files via upload

---
+++ ClassMain.java
@@ -0,0 +1,20 @@
+package tema1;
+
+import java.util.StringTokenizer;
+
+public class ClassMain {
+    public static void main(String[] args) {
+        int numberInt = 15;
+        long numberLong = 92545855;
+        double numberDouble = 1.854;
+        boolean CharacterBolean = true;
+        String characterString = "hellow world";
+        
+        System.out.println("numeros enteros: " + numberInt);
+        System.out.println("numeros muy largos: " + numberLong);
+        System.out.println("numeros con decimales: " + numberDouble);
+        System.out.println("caracter boleano falso o verdadero: " + CharacterBolean);
+        System.out.println("caracter tipo texto: " + characterString);
+
+    }
+}
